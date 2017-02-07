8f7369e852349705b47eae0dd5e73628b5049e6f
Wembassy <mitzi.bootyard@gmail.com>
2017-02-07 15:17:39 +0800

    Creating Hamburger animation

--- app/shared/header/header.directive.js
+++ app/shared/header/header.directive.js
@@ -1,6 +1,6 @@
-angular.module('longPlay').directive('header', [headerDirective]);
+angular.module('longPlay').directive('header', ['$rootScope', headerDirective]);
 
-function headerDirective() {
+function headerDirective($rootScope) {
   return {
     restrict: 'A',
     controller: function($scope, Menu) {
@@ -15,6 +15,14 @@ function headerDirective() {
     link: function(scope, element, attrs) {
       angular.element('.hamburger-box').click(function() {
         angular.element('.hamburger-box').toggleClass('active');
+
+        if ( angular.element('.hamburger-box').hasClass('active') ) {
+          $rootScope.$broadcast('menu-opened');
+        }
+        else {
+          $rootScope.$broadcast('menu-closed');
+        }
+
       });
     },
     templateUrl: 'app/shared/header/header.tpl.html'
